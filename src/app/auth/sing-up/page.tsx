9d93768f7c48652a34956b2890848d965a3de1ea
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-05-15 22:13:59 -0300

    initial commit

---
+++ src/app/auth/sing-up/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '../sing-in/_components/sing-in-form'
+
+export default function SingUp() {
+  return (
+    <div className='h-screen flex items-center'>
+      <LoginForm />
+    </div>
+  )
+}
