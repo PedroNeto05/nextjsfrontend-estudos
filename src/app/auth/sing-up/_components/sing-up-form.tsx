ed750e9336ce5efefb79b9f84bf4fa7c8a5bb69f
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-05-16 11:35:10 -0300

    init singup page

--- src/app/auth/sing-up/_components/sing-up-form.tsx
+++ src/app/auth/sing-up/_components/sing-up-form.tsx
@@ -1,5 +1,5 @@
+'use client'
 import Link from 'next/link'
-
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -9,9 +9,55 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const singUpFormSchema = z
+  .object({
+    firstName: z
+      .string()
+      .trim()
+      .min(4, 'First Name must be at least 4 characters.'),
+    lastName: z
+      .string()
+      .trim()
+      .min(4, 'Last Name must be at least 4 characters.'),
+    email: z.string().trim().email(),
+    confirmEmail: z.string().trim(),
+    password: z
+      .string()
+      .trim()
+      .min(6, 'Password must be at least 4 characters.'),
+    confirmPassword: z.string().trim(),
+  })
+  .refine((data) => data.email === data.confirmEmail, {
+    message: 'Emails not match',
+    path: ['confirmEmail'],
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords not match',
+    path: ['confirmPassword'],
+  })
+
+type SingUpFormSchema = z.infer<typeof singUpFormSchema>
 
 export function SingUpForm() {
+  const form = useForm<SingUpFormSchema>({
+    resolver: zodResolver(singUpFormSchema),
+  })
+
+  function handleSingUp(data: SingUpFormSchema) {
+    console.log(data)
+  }
+
   return (
     <Card className='mx-auto max-w-sm'>
       <CardHeader>
@@ -21,47 +67,108 @@ export function SingUpForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form className='grid gap-4'>
-          <div className='grid grid-cols-2 gap-4'>
-            <div className='grid gap-2'>
-              <Label htmlFor='first-name'>First name</Label>
-              <Input id='first-name' placeholder='Pedro' required />
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSingUp)}
+            className='grid gap-4'
+          >
+            <div className='grid grid-cols-2 gap-4'>
+              <FormField
+                control={form.control}
+                name='firstName'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Pedro' {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='lastName'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <div>
+                        <Input placeholder='Neto' {...field} />
+                      </div>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
             </div>
             <div className='grid gap-2'>
-              <Label htmlFor='last-name'>Last name</Label>
-              <Input id='last-name' placeholder='Neto' required />
+              <FormField
+                control={form.control}
+                name='email'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type='email'
+                        placeholder='m@example.com'
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
             </div>
-          </div>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Email</Label>
-            <Input
-              id='email'
-              type='email'
-              placeholder='m@example.com'
-              required
-            />
-          </div>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Confirm Email</Label>
-            <Input
-              id='email'
-              type='email'
-              placeholder='m@example.com'
-              required
-            />
-          </div>
-          <div className='grid gap-2'>
-            <Label htmlFor='password'>Password</Label>
-            <Input id='password' type='password' />
-          </div>
-          <div className='grid gap-2'>
-            <Label htmlFor='password'>Confirm Password</Label>
-            <Input id='password' type='password' />
-          </div>
-          <Button type='submit' className='w-full'>
-            Create an account
-          </Button>
-        </form>
+            <div className='grid gap-2'>
+              <FormField
+                control={form.control}
+                name='confirmEmail'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type='email'
+                        placeholder='m@example.com'
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='grid gap-2'>
+              <FormField
+                control={form.control}
+                name='password'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type='password' {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='grid gap-2'>
+              <FormField
+                control={form.control}
+                name='confirmPassword'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input type='password' {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button type='submit' className='w-full'>
+              Create an account
+            </Button>
+          </form>
+        </Form>
         <div className='mt-4 text-center text-sm'>
           Already have an account?{' '}
           <Link href='#' className='underline'>
