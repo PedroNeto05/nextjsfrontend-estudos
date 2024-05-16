34a7a6e6646b4911ad24d0f9666f5e633f646c4b
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-05-16 18:48:56 -0300

    feat: home path

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,41 @@
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Task } from './_components/task'
+import { UserNav } from './_components/user-nav'
+import { SquareCheck } from 'lucide-react'
+
+const teste =
+  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat essequaerat doloribus. Vel ipsa molestiae illo, eius ut praesentium animi, quisvoluptas, voluptatum tenetur velit excepturi ex alias quas nulla! Loremipsum dolor, sit amet consectetur adipisicing elit. Fugiat esse quaeratdoloribus. Vel ipsa molestiae illo, eius ut praesentium animi, quisvoluptas, voluptatum tenetur velit excepturi ex alias quas nulla! Loremipsum dolor, sit amet consectetur adipisicing elit. Fugiat esse quaeratdoloribus. Vel ipsa molestiae illo, eius ut praesentium animi, quisvoluptas, voluptatum tenetur velit excepturi ex alias quas nulla! Loremipsum dolor, sit amet consectetur adipisicing elit. Fugiat esse quaeratdoloribus. Vel ipsa molestiae illo, eius ut praesentium animi, quisvoluptas, voluptatum tenetur velit excepturi ex alias quas nulla! Loremipsum dolor, sit amet consectetur adipisicing elit. Fugiat esse quaeratdoloribus. Vel ipsa molestiae illo, eius ut praesentium animi, quisvoluptas, voluptatum tenetur velit excepturi ex alias quas nulla! Loremipsum dolor, sit amet consectetur adipisicing elit. Fugiat esse quaeratdoloribus. Vel ipsa molestiae illo, eius ut praesentium animi, quisvoluptas, voluptatum tenetur velit excepturi ex alias quas nulla! Loremipsum dolor, sit amet consectetur adipisicing elit. Fugiat esse quaeratdoloribus. Vel ipsa molestiae illo, eius ut praesentium animi, quisvoluptas, voluptatum tenetur velit excepturi ex alias quas nulla! Loremipsum dolor, sit amet consectetur adipisicing elit. Fugiat esse quaeratdoloribus. Vel ipsa molestiae illo, eius ut praesentium animi, quisvoluptas, voluptatum tenetur velit excepturi ex alias quas nulla!'
+
+//
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <div className='flex flex-col h-screen'>
+      <div className='flex justify-between p-4 border-b border-border items-center shadow-md '>
+        <SquareCheck className='size-10' />
+        <UserNav />
+      </div>
+      <ScrollArea className='h-full p-10 pt-3 pb-0'>
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+        <Task title='teste' description={teste} />
+      </ScrollArea>
+    </div>
+  )
 }
