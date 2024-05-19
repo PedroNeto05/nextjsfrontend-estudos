d5864dcde9968b337c352888da05175049481f6b
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-05-18 21:09:06 -0300

    feat: change title and descripiton task

--- src/app/home/_components/task.tsx
+++ src/app/home/_components/task.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import {
@@ -9,7 +11,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import { Trash2 } from 'lucide-react'
+import { useState } from 'react'
 
 type TaskProps = {
   title: string
@@ -17,29 +22,42 @@ type TaskProps = {
 }
 
 export function Task(props: TaskProps) {
+  const [description, setDescription] = useState(props.description)
+  const [title, setTitle] = useState(props.title)
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Card className='p-4 flex items-center gap-8  overflow-hidden mb-4 cursor-pointer min-h-24'>
+      <Card className='p-4 flex items-center gap-8  overflow-hidden mb-4 cursor-pointer min-h-24 hover:bg-slate-100'>
+        <DialogTrigger asChild>
           <div className='w-full overflow-hidden max-h-24 relative select-none'>
             <h3 className='scroll-m-20 text-2xl font-semibold tracking-tight'>
-              {props.title}
+              {title}
             </h3>
-            <p>{props.description}</p>
+            <p>{description}</p>
             <div className='pointer-events-none absolute bottom-0 left-0 right-0 h-[10%] bg-gradient-to-t from-white/100 to-white/10' />
           </div>
-          <Card className='border border-border p-4 cursor-pointer'>
-            <Trash2 className='text-red-600' />
-          </Card>
+        </DialogTrigger>
+        <Card className='border border-border p-4 cursor-pointer'>
+          <Trash2 className='text-red-600' />
         </Card>
-      </DialogTrigger>
-      <DialogContent className='max-w-4xl'>
+      </Card>
+      <DialogContent className=''>
         <DialogHeader>
-          <DialogTitle className='text-4xl'>{props.title}</DialogTitle>
+          <DialogTitle>
+            <Input
+              value={title}
+              className='text-4xl outline-none border-transparent'
+              onChange={(e) => setTitle(e.currentTarget.value)}
+            />
+          </DialogTitle>
         </DialogHeader>
-        <div className='flex items-center'>
-          <textarea className='size-full '>{props.description}</textarea>
-        </div>
+
+        <Textarea
+          className='size-full'
+          value={description}
+          onChange={(e) => setDescription(e.currentTarget.value)}
+        />
+
         <DialogFooter className='justify-end'>
           <DialogClose asChild>
             <Button type='button'>Salvar</Button>
