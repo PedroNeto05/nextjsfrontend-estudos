bd0e4203e96f3dd1479fc38ca2d7859129ec70e0
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-05-21 15:49:45 -0300

    router middleware

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const singInPath = '/auth/sing-in'
+  const singUpPath = '/auth/sing-up'
+  const homePath = '/home'
+  const token = req.cookies.get('yourCookieName')
+
+  console.log(req.url)
+
+  if (
+    !token &&
+    req.nextUrl.pathname !== singInPath &&
+    req.nextUrl.pathname !== singUpPath
+  ) {
+    return NextResponse.redirect(new URL(singInPath, req.url))
+  }
+
+  if (
+    token &&
+    (req.nextUrl.pathname === singInPath ||
+      req.nextUrl.pathname === singUpPath ||
+      req.nextUrl.pathname === '/')
+  ) {
+    return NextResponse.redirect(new URL(homePath, req.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|auth/sign-in|auth/sign-up).*)',
+  ],
+}
